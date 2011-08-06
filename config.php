a6b1e4c01c84e255e33c66b361199d83388ef223
Brian Hendrickson <brian@megapump.com>
2011-08-06 01:59:55 -0700

    fix config tabs

--- config.php
+++ config.php
@@ -2,17 +2,17 @@
 
 // USE A REMOTE DATABASE (at datamullet.com or other host)
 
-define( 'REMOTE_HOST',				'https://datamullet.com'	);
-define( 'REMOTE_USER',				'guest'										);
-define( 'REMOTE_PASSWORD',		'guest'										);
+define( 'REMOTE_HOST',        'https://datamullet.com');
+define( 'REMOTE_USER',        'guest');
+define( 'REMOTE_PASSWORD',    'guest');
 
 
 // OR USE A LOCAL DATABASE
 
-define( 'DATABASE_ENGINE',		'mongodb'									); // mysql, pgsql
-define( 'DATABASE_USER',			'ben'											);
-define( 'DATABASE_PASSWORD',	'example'									);
-define( 'DATABASE_NAME',			'testdb'									);
-define( 'DATABASE_HOST',			'localhost'								); // or leave it blank
-define( 'DATABASE_PORT',			5432											); // 3306 for MySQL
+define( 'DATABASE_ENGINE',    'mongodb'); // mysql, pgsql
+define( 'DATABASE_USER',      'ben');
+define( 'DATABASE_PASSWORD',  'example');
+define( 'DATABASE_NAME',      'testdb');
+define( 'DATABASE_HOST',      'localhost'); // or leave it blank
+define( 'DATABASE_PORT',      5432); // 3306 for MySQL
 
